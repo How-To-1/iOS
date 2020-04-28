3d9d7eec11157c31cb22b31092155d497349d61f
Dahna Buenrostro <dbuenrostro09@gmail.com>
2020-04-28 18:04:10 -0400

    Created CoreData models for user and tutorial

---
+++ HowTo/HowTo/Tutorials.swift
@@ -0,0 +1,30 @@
+//
+//  Tutorials.swift
+//  HowTo
+//
+//  Created by Dahna on 4/28/20.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Tutorials")
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
