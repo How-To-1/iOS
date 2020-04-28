b95274357abfbfa418ef984a4c72406f4f5e7a51
Dahna Buenrostro <dbuenrostro09@gmail.com>
2020-04-28 18:04:51 -0400

    Created Tutorial Representaion

---
+++ HowTo/HowTo/TutorialRepresentation.swift
@@ -0,0 +1,16 @@
+//
+//  TutorialRepresentation.swift
+//  HowTo
+//
+//  Created by Dahna on 4/28/20.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import Foundation
+
+struct TutorialRepresentation: Codable {
+    var title: String
+    var guide: String
+    var Category: String
+    var id: UUID
+}
