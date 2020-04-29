dfa659e6b2f2288d049de4845580a4de20374d35
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-29 22:00:52 +0100

    HomeScreenTableViewController Created, with Outlets and Properties

---
+++ HowTo/HowTo/HomeScreenTableViewController.swift
@@ -0,0 +1,75 @@
+//
+//  HomeScreenTableViewController.swift
+//  HowTo
+//
+//  Created by Tobi Kuyoro on 29/04/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenTableViewController: UITableViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var guideTitleLabel: UILabel!
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    @IBOutlet weak var signOutButton: UIBarButtonItem!
+    @IBOutlet weak var searchBar: UISearchBar!
+
+    // MARK: - Properties
+
+    let userController = UserController()
+    let tutorialController = TutorialController()
+
+    // MARK: - Actions
+
+
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+}
