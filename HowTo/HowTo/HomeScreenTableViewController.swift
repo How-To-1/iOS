56b6d92153529d48d3fd4a22609f678bfb4122da
Dahna Buenrostro <dbuenrostro09@gmail.com>
2020-04-29 17:21:52 -0400

    Started on HomeScreenController and reset a few outlets

--- HowTo/HowTo/HomeScreenTableViewController.swift
+++ HowTo/HowTo/HomeScreenTableViewController.swift
@@ -7,14 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class HomeScreenTableViewController: UITableViewController {
 
     // MARK: - Outlets
 
-    @IBOutlet weak var categoryLabel: UILabel!
-    @IBOutlet weak var guideTitleLabel: UILabel!
-    @IBOutlet weak var categoryImage: UIImageView!
+
     @IBOutlet weak var addButton: UIBarButtonItem!
     @IBOutlet weak var signOutButton: UIBarButtonItem!
     @IBOutlet weak var searchBar: UISearchBar!
@@ -23,6 +22,20 @@ class HomeScreenTableViewController: UITableViewController {
 
     let userController = UserController()
     let tutorialController = TutorialController()
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Tutorial> = {
+        let fetchRequest: NSFetchRequest<Tutorial> = Tutorial.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "category", ascending: true),
+                                        NSSortDescriptor(key: "title", ascending: true)]
+        let context = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                             managedObjectContext: context,
+                                             sectionNameKeyPath: "category",
+                                             cacheName: nil)
+        frc.delegate = self
+        try! frc.performFetch()
+        return frc
+    }()
 
     // MARK: - Actions
 
@@ -40,11 +53,11 @@ class HomeScreenTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return fetchedResultsController.sections?.count ?? 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -71,5 +84,54 @@ class HomeScreenTableViewController: UITableViewController {
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
+    }
+}
+
+extension HomeScreenTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            break
+        }
     }
 }
---
+++ HowTo/HowTo/TutorialTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialTableViewCell.swift
+//  HowTo
+//
+//  Created by Dahna on 4/29/20.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class TutorialTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var guideTitleLabel: UILabel!
+    @IBOutlet weak var categoryImage: UIImageView!
+}
+
