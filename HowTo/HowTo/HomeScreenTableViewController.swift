db48d6e7aac99a5f2800198fd5483fb65ff46a77
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-30 17:12:06 +0100

    Changes to HomeScreenTableViewController

--- HowTo/HowTo/HomeScreenTableViewController.swift
+++ HowTo/HowTo/HomeScreenTableViewController.swift
@@ -19,14 +19,52 @@ class HomeScreenTableViewController: UITableViewController {
 
     // MARK: - Properties
 
-    var frc = NSFetchedResultsController<Tutorial>()
+//    var frc = NSFetchedResultsController<Tutorial>()
     
     let userController = UserController()
     let tutorialController = TutorialController()
 
+    lazy var fetchedResultsController: NSFetchedResultsController<Tutorial> = {
+        let fetchRequest: NSFetchRequest<Tutorial> = Tutorial.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+
+        if searchBar.text != "" {
+            fetchRequest.predicate = NSPredicate(format: "title CONTAINS[c] %@", searchBar.text ?? "")
+        }
+
+        let context = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                             managedObjectContext: context,
+                                             sectionNameKeyPath: "title",
+                                             cacheName: nil)
+        frc.delegate = self
+
+        do {
+            try frc.performFetch()
+        } catch {
+            fatalError("Error performing fetch:\(error)")
+        }
+
+        return frc
+    }()
+
     // MARK: - Actions
 
     @IBAction func addButtonTapped(_ sender: Any) {
+    }
+
+    func titleView() {
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: 50, height: 40))
+        label.backgroundColor = .clear
+        label.font = UIFont(name: "Play-Bold", size: 22)
+
+        label.text = "Guides"
+        label.numberOfLines = 2
+        label.textColor = .black
+        label.sizeToFit()
+        label.textAlignment = .center
+
+        self.navigationItem.titleView = label
     }
 
     // MARK: - View Lifecycle
@@ -34,7 +72,8 @@ class HomeScreenTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
-        setupFRC()
+        titleView()
+        //        setupFRC()
         
         if userController.bearer == nil {
             self.navigationItem.leftBarButtonItem = nil
@@ -45,31 +84,33 @@ class HomeScreenTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let objects = frc.fetchedObjects else { return 0 }
-        return objects.count
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       guard let cell = tableView.dequeueReusableCell(withIdentifier: TutorialTableViewCell.reuseIdentifier, for: indexPath) as? TutorialTableViewCell else {
+       guard let cell = tableView.dequeueReusableCell(withIdentifier: "TutorialCell", for: indexPath) as? TutorialTableViewCell else {
             fatalError("Can't dequeue cell of type TutorialCell")
         }
          
-        cell.tutorial = frc.object(at: indexPath)
+        let tutorial = fetchedResultsController.object(at: indexPath)
+        cell.tutorial = tutorial
 
         return cell
     }
 
-
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let tutorial = frc.object(at: indexPath)
+            let tutorial = fetchedResultsController.object(at: indexPath)
             tutorialController.deleteTutorialFromServer(tutorial: tutorial)
             tutorialController.delete(tutorial: tutorial)
         }
     }
-
 
     // MARK: - Navigation
 
@@ -79,40 +120,89 @@ class HomeScreenTableViewController: UITableViewController {
                 destinationVC.userController = userController
                 destinationVC.tutorialController = tutorialController
             }
-        }
-
-        else if segue.identifier == "ShowTutorialDetailSegue" {
+        } else if segue.identifier == "ShowTutorialDetailSegue" {
 
         }
     }
     
     // MARK: - Private Functions
     
-    func setupFRC() {
-        let request: NSFetchRequest<Tutorial> = Tutorial.fetchRequest()
-        // filter what we want from CoreData
-        if searchBar.text != "" {
-            request.predicate = NSPredicate(format: "title CONTAINS[c] %@", searchBar.text ?? "")
+//    func setupFRC() {
+//        let request: NSFetchRequest<Tutorial> = Tutorial.fetchRequest()
+//        // filter what we want from CoreData
+//        if searchBar.text != "" {
+//            request.predicate = NSPredicate(format: "title CONTAINS[c] %@", searchBar.text ?? "")
+//        }
+//        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+//        let moc = CoreDataStack.shared.mainContext
+//        let fetchedResultsController = NSFetchedResultsController(fetchRequest: request,
+//                                                                  managedObjectContext: moc,
+//                                                                  sectionNameKeyPath: nil,
+//                                                                  cacheName: nil)
+//        do {
+//            try fetchedResultsController.performFetch()
+//        } catch {
+//            print("could not fetch: \(error)")
+//        }
+//        frc = fetchedResultsController
+//        tableView.reloadData()
+//    }
+}
+
+extension HomeScreenTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let indexPath = indexPath,
+                let newIndexPath = newIndexPath else { return }
+            tableView.moveRow(at: indexPath, to: newIndexPath)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        @unknown default:
+            return
         }
-        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-        let moc = CoreDataStack.shared.mainContext
-        let fetchedResultsController = NSFetchedResultsController(fetchRequest: request,
-                                                                  managedObjectContext: moc,
-                                                                  sectionNameKeyPath: nil,
-                                                                  cacheName: nil)
-        do {
-            try fetchedResultsController.performFetch()
-        } catch {
-            print("could not fetch: \(error)")
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+
+        let sectionSet = IndexSet(integer: sectionIndex)
+
+        switch type {
+        case .insert:
+            tableView.insertSections(sectionSet, with: .automatic)
+        case .delete:
+            tableView.deleteSections(sectionSet, with: .automatic)
+        default:
+            return
         }
-        frc = fetchedResultsController
-        tableView.reloadData()
     }
 }
 
 extension HomeScreenTableViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        setupFRC()
+//        setupFRC()
     }
 }
